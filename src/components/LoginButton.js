d07b900c7fb3d393f1c61419d8fb14019fedc4c4
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-06-04 12:21:39 -0400

    Complete Login styling

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import { Button, Icon } from 'semantic-ui-react';
 
 class LoginButton extends Component {
   render() {
     return (
       <Button
-        
         href={`http://localhost:3001/api/v1/meetup`}
-      >{' '}
+        size="massive"
+        className="ui red change"
+        color="pink"
+      ><Icon name='meetup' />
       Login with Meetup
       </Button>
     );
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import LoginButton from '../components/LoginButton';
+import { Segment, Dimmer } from 'semantic-ui-react'
+import './Login.css';
 
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
-        <div className="Login-header">
-          <h1 className="oh-haay-logo">oh-haay!</h1>
-        </div>
-        <p className="Login-intro">
-          An app to to connect better on meetup.
-          </p>
-        <div className="Login-buttons">
-          <LoginButton />
-        </div>
-      </div>
+      <div className="login">
+        <Dimmer.Dimmable as={Segment} dimmed
+          textAlign='center'
+          style={{ minHeight: 300, padding: '1em 0em' }}
+          vertical
+          className="dimme"
+        >
+
+          <div className="login-header">
+            <h1 className="oh-haay-logo-home">oh-haay!</h1>
+          </div>
+          <p className="login-intro">
+            Connect better on meetup.
+           </p>
+          <div className="login-buttons">
+            <LoginButton />
+          </div>
+        
+        </Dimmer.Dimmable>
+      </div >
+      // <div className="Login">
+      //   <div className="Login-header">
+      //     <h1 className="oh-haay-logo">oh-haay!</h1>
+      //   </div>
+      //   <p className="Login-intro">
+      //     An app to to connect better on meetup.
+      //     </p>
+      //   <div className="Login-buttons">
+      //     <LoginButton />
+      //   </div>
+      // </div>
     );
   }
 }
