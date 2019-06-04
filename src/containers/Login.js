86409bc8fc415b261f8d84434d470bd3f3795db4
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-06-04 10:46:03 -0400

    Add background image, begin styling Login Page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,13 +6,11 @@ class Login extends Component {
     return (
       <div className="Login">
         <div className="Login-header">
-          <h2 className="Login-title">
-            oh-haay!
-          </h2>
-          <p className="Login-intro">
-            An app to to connect better on meetup.
-          </p>
+          <h1 className="oh-haay-logo">oh-haay!</h1>
         </div>
+        <p className="Login-intro">
+          An app to to connect better on meetup.
+          </p>
         <div className="Login-buttons">
           <LoginButton />
         </div>
